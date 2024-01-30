ab1158258ca50e84c643ef37e6dc1c511021e234
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-30 01:53:58 +0100

    handles conversion specifier

--- functions1.c
+++ functions1.c
@@ -5,8 +5,8 @@
  * print_unsigned - Prints an unsigned number
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
+ * @flags:  To calculate active flags
+ * @width: to get width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed.
